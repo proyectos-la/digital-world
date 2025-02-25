803c14d8cf7c27bb0d77aab46395cb6070a9d87c
Luis Aramayo <luis.aramayo.18@gmail.com>
2025-02-25 01:49:43 -0300

    probando

--- ecommer_electronica_backend2/settings.py
+++ ecommer_electronica_backend2/settings.py
@@ -9,7 +9,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = ["*"]
 
 # Apps y middleware
 INSTALLED_APPS = [
