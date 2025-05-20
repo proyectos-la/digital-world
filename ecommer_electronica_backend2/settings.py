87ff2e9f6d86a952c8fc3b4c006ab0c5a01dd4f1
luis <luisss.18@hotmail.com>
2025-05-19 23:46:03 -0300

    update setting config

--- ecommer_electronica_backend2/settings.py
+++ ecommer_electronica_backend2/settings.py
@@ -16,7 +16,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+ALLOWED_HOSTS = env.bool('DEBUG', default=False)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
